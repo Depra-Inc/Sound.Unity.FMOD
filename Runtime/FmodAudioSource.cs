3a0cd2a7af2bf9f1b24887c9fbb45607ef3fb7ff
godzzz <g0dzZz1lla@yandex.ru>
2024-04-28 06:16:53 +0400

    [v0.0.3] Dependencies updated

--- Runtime/FmodAudioSource.cs
+++ Runtime/FmodAudioSource.cs
@@ -1,7 +1,9 @@
 ﻿// SPDX-License-Identifier: Apache-2.0
 // © 2024 Nikolay Melnikov <n.melnikov@depra.org>
 
+using System;
 using Depra.Sound.Clip;
+using Depra.Sound.Parameter;
 using Depra.Sound.Source;
 using FMOD;
 using FMOD.Studio;
@@ -14,18 +16,14 @@ namespace Depra.Sound.FMOD
 	public sealed class FmodAudioSource : MonoBehaviour, IAudioSource
 	{
 		[SerializeField] private STOP_MODE _stopMode;
-
 		private EventInstance _lastInstance;
+
+		public event IAudioSource.PlayDelegate Started;
+		public event IAudioSource.StopDelegate Stopped;
 
 		public bool IsPlaying => _lastInstance.isValid() &&
 		                         _lastInstance.getPlaybackState(out var state) == RESULT.OK &&
 		                         state == PLAYBACK_STATE.PLAYING;
-
-		float IAudioSource.Volume
-		{
-			get => RuntimeManager.GetBus("bus:/").getVolume(out var volume) == RESULT.OK ? volume : 0f;
-			set => RuntimeManager.GetBus("bus:/").setVolume(value);
-		}
 
 		private void OnDestroy()
 		{
@@ -47,6 +45,41 @@ namespace Depra.Sound.FMOD
 			if (IsPlaying)
 			{
 				_lastInstance.stop(_stopMode);
+			}
+		}
+
+		public IAudioClipParameter GetParameter(Type type)
+		{
+			if (_lastInstance.isValid() == false)
+			{
+				return new NullParameter();
+			}
+
+			return type switch
+			{
+				_ when type == typeof(VolumeParameter) && _lastInstance.getVolume(out var volume) == RESULT.OK =>
+					new VolumeParameter(volume),
+				_ when type == typeof(PitchParameter) && _lastInstance.getPitch(out var pitch) == RESULT.OK =>
+					new PitchParameter(pitch),
+				_ => new NullParameter()
+			};
+		}
+
+		public void SetParameter(IAudioClipParameter parameter)
+		{
+			if (_lastInstance.isValid() == false)
+			{
+				return;
+			}
+
+			switch (parameter)
+			{
+				case VolumeParameter volume:
+					_lastInstance.setVolume(volume.Value);
+					break;
+				case PitchParameter pitch:
+					_lastInstance.setPitch(pitch.Value);
+					break;
 			}
 		}
 	}
