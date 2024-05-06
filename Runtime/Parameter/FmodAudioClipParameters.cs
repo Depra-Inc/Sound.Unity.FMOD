046ae76b41ef33850fd8e0fad5dda141fc965ba2
godzzz <g0dzZz1lla@yandex.ru>
2024-05-06 19:06:32 +0400

    [v0.0.9] Supported FMOD labeled parameter

--- Runtime/Parameter/FmodAudioClipParameters.cs
+++ Runtime/Parameter/FmodAudioClipParameters.cs
@@ -5,8 +5,10 @@ using System;
 using System.Collections.Generic;
 using Depra.Sound.Parameter;
 using FMOD;
+using FMOD.Studio;
 using FMODUnity;
 using UnityEngine;
+using Debug = UnityEngine.Debug;
 
 namespace Depra.Sound.FMOD
 {
@@ -15,6 +17,28 @@ namespace Depra.Sound.FMOD
 		private readonly FmodAudioSource _source;
 
 		public FmodAudioClipParameters(FmodAudioSource source) => _source = source;
+
+		private IAudioClipParameter GetInternal(EventInstance instance, Type type) => type switch
+		{
+			_ when type == typeof(VolumeParameter) && instance.getVolume(out var volume) == RESULT.OK =>
+				new VolumeParameter(volume),
+			_ when type == typeof(PitchParameter) && instance.getPitch(out var pitch) == RESULT.OK =>
+				new PitchParameter(pitch),
+			_ when type == typeof(PositionParameter) && instance.get3DAttributes(out var attr) == RESULT.OK =>
+				new PositionParameter(new Vector3(attr.position.x, attr.position.y, attr.position.z)),
+			_ => new NullParameter()
+		};
+
+		private RESULT SetInternal(EventInstance instance, IAudioClipParameter parameter) => parameter switch
+		{
+			VolumeParameter volume => instance.setVolume(volume.Value),
+			PitchParameter pitch => instance.setPitch(pitch.Value),
+			PositionParameter position => instance.set3DAttributes(position.Value.To3DAttributes()),
+			FmodFloat @float => instance.setParameterByName(@float.Name, @float.Value, @float.IgnoreSeekSpeed),
+			FmodInteger integer => instance.setParameterByName(integer.Name, integer.Value, integer.IgnoreSeekSpeed),
+			FmodLabel label => instance.setParameterByNameWithLabel(label.Name, label.Value, label.IgnoreSeekSpeed),
+			_ => RESULT.ERR_INVALID_PARAM
+		};
 
 		IEnumerable<Type> IAudioClipParameters.SupportedTypes() => new[]
 		{
@@ -28,47 +52,21 @@ namespace Depra.Sound.FMOD
 		IAudioClipParameter IAudioClipParameters.Get(Type type)
 		{
 			var instance = _source.LastInstance;
-			if (instance.isValid() == false)
-			{
-				return new NullParameter();
-			}
-
-			return type switch
-			{
-				_ when type == typeof(VolumeParameter) && instance.getVolume(out var volume) == RESULT.OK =>
-					new VolumeParameter(volume),
-				_ when type == typeof(PitchParameter) && instance.getPitch(out var pitch) == RESULT.OK =>
-					new PitchParameter(pitch),
-				_ when type == typeof(PositionParameter) && instance.get3DAttributes(out var attr) == RESULT.OK =>
-					new PositionParameter(new Vector3(attr.position.x, attr.position.y, attr.position.z)),
-				_ => new NullParameter()
-			};
+			return instance.isValid() ? GetInternal(instance, type) : new NullParameter();
 		}
 
 		void IAudioClipParameters.Set(IAudioClipParameter parameter)
 		{
-			if (_source.LastInstance.isValid() == false)
+			var instance = _source.LastInstance;
+			if (instance.isValid() == false)
 			{
 				return;
 			}
 
-			switch (parameter)
+			var result = SetInternal(instance, parameter);
+			if (result != RESULT.OK)
 			{
-				case VolumeParameter volume:
-					_source.LastInstance.setVolume(volume.Value);
-					break;
-				case PitchParameter pitch:
-					_source.LastInstance.setPitch(pitch.Value);
-					break;
-				case PositionParameter position:
-					_source.LastInstance.set3DAttributes(position.Value.To3DAttributes());
-					break;
-				case FmodFloat @float:
-					_source.LastInstance.setParameterByName(@float.Name, @float.Value, @float.IgnoreSeekSpeed);
-					break;
-				case FmodInteger integer:
-					_source.LastInstance.setParameterByName(integer.Name, integer.Value, integer.IgnoreSeekSpeed);
-					break;
+				Debug.LogError($"Failed to set parameter: {result}");
 			}
 		}
 	}
---
+++ Runtime/Parameter/FmodLabel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Depra.Sound.Parameter;
+
+namespace Depra.Sound.FMOD
+{
+	[Serializable]
+	public struct FmodLabel : IAudioClipParameter
+	{
+		public string Name;
+		public string Value;
+		public bool IgnoreSeekSpeed;
+
+		public FmodLabel(string name, string value, bool ignoreSeekSpeed = false)
+		{
+			Name = name;
+			Value = value;
+			IgnoreSeekSpeed = ignoreSeekSpeed;
+		}
+	}
+}
